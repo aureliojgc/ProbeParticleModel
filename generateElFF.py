cb536dcd5b89cdfddddc70e88a6e9f11bdfb6d83
aureliojgc <aureliogaca@gmail.com>
2021-02-26 14:47:27 +0100

    a

--- generateElFF.py
+++ generateElFF.py
@@ -212,24 +212,14 @@ if __name__=="__main__":
             rho_tip_kpfm, lvec_tip, nDim_tip, head_tip = GU.loadCUBE( options.KPFM_tip, hartree=False )
             drho_kpfm = (rho_tip_kpfm - rho_tip_v0_aux)#/(options.Vref)
         if options.KPFM_tip in {'fit', 'dipole', 'pz'}:
-            print " "
-            print " "
-            print " I do at leats get that tip is a fit"
-            print " "
-            print PPU.params['probeType']
-            print " "
             if ( PPU.params['probeType'] == '8' ):
                 drho_kpfm={'pz':0.025} # compared with DFT VASP 0.015. As VASP goes with q=-1.0 and dz2 goes with -0.2 -> 5*0.015. COAg tip is over 0.025, so 0.125
                 sigma = 0.7
-                print "  "
-                print "  "
-                print " I get you want CO-tip !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  "
-                print "  "
-                print "  "
-                print "  "
-            if ( PPU.params['probeType'] == 24 ):
+                print " Select CO-tip polarization "
+            if ( PPU.params['probeType'] == '24' ):
                 drho_kpfm={'pz':0.016}
                 sigma = 0.8
+                print " Select Xe-tip polarization "
                         #Calculate ~V terms
         FFkpfm_t0sV,Eel_t0sV=PPH.computeElFF(dV_kpfm,lvec,nDim,PPU.params['tip'],computeVpot=options.energy , tilt=opt_dict['tilt'] ,)
         FFkpfm_tVs0,Eel_tVs0=PPH.computeElFF(V_v0_aux2,lvec,nDim,drho_kpfm,computeVpot=options.energy , tilt=opt_dict['tilt'] )
