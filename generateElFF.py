56cc43a480644d14340987d00f50118fbd119543
aureliojgc <aureliogaca@gmail.com>
2021-02-26 13:31:55 +0100

    a

--- generateElFF.py
+++ generateElFF.py
@@ -208,7 +208,7 @@ if __name__=="__main__":
             rho_tip_kpfm, lvec_tip, nDim_tip, head_tip = GU.loadCUBE( options.KPFM_tip, hartree=False )
             drho_kpfm = (rho_tip_kpfm - rho_tip_v0_aux)#/(options.Vref)
         elif options.KPFM_tip in {'fit', 'dipole', 'pz'}:
-            if ( PPU.params['probeType'] == 'O' ):
+            if ( PPU.params['probeType'] is 'O' ):
                 drho_kpfm={'pz':0.025} # compared with DFT VASP 0.015. As VASP goes with q=-1.0 and dz2 goes with -0.2 -> 5*0.015. COAg tip is over 0.025, so 0.125
                 sigma = 0.7
             if ( PPU.params['probeType'] == 'Xe' ):
