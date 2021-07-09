fd830e278137a951b70348a7c527af6b2ba95c75
Aurelio Jesus Gallardo Caparros <aureliogaca@luna.fzu.cz>
2021-07-09 10:07:27 +0200

    just adjusting the pz pol for kpfm

--- generateElFF.py
+++ generateElFF.py
@@ -153,19 +153,19 @@ if __name__=="__main__":
             Vref = -0.1
             if ( PPU.params['probeType'] == '8' ):
                 #drho_kpfm={'pz':0.095} # comparing charges, 0.09 * 0.2 looks the same as COAg111
-                #pol_sigma = 0.6
+                #sigma = 0.6
                 #drho_kpfm={'pz':0.125} # compared with DFT VASP 0.015. As VASP goes with q=-1.0 and dz2 goes with -0.2 -> 5*0.015. COAg tip is over 0.025, so 0.125
-                #pol_sigma = 0.7
+                #sigma = 0.7
                 drho_kpfm={'pz':0.180} # compared with DFT VASP 0.015. As VASP goes with q=-1.0 and dz2 goes with -0.2 -> 5*0.015. COAg tip is over 0.025, so 0.125
-                pol_sigma = 0.4
+                sigma = 0.4
                 print " Select CO-tip polarization "
             if ( PPU.params['probeType'] == '47' ):
                 drho_kpfm={'pz':0.21875} 
-                pol_sigma = 0.7
+                sigma = 0.7
                 print " Select Ag polarization with decay sigma", sigma
             if ( PPU.params['probeType'] == '54' ):
                 drho_kpfm={'pz':0.250}
-                pol_sigma = 0.8
+                sigma = 0.8
                 print " Select Xe-tip polarization"
                         #Calculate ~V terms     
         if options.tip_dens is not None:
@@ -173,7 +173,7 @@ if __name__=="__main__":
         else:
             tip_aux_2 = PPU.params['tip']
         FFkpfm_t0sV,Eel_t0sV=PPH.computeElFF(dV_kpfm,lvec,nDim,tip_aux_2,computeVpot=options.energy , tilt=opt_dict['tilt'] ,)
-        FFkpfm_tVs0,Eel_tVs0=PPH.computeElFF(V_v0_aux2,lvec,nDim,drho_kpfm,computeVpot=options.energy , tilt=opt_dict['tilt'], sigma=pol_sigma )
+        FFkpfm_tVs0,Eel_tVs0=PPH.computeElFF(V_v0_aux2,lvec,nDim,drho_kpfm,computeVpot=options.energy , tilt=opt_dict['tilt'], sigma=sigma )
 
         #debug save tippol
         if options.KPFM_tip in {'fit', 'dipole', 'pz'}:
